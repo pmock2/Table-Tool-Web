d6f3680a7c4bca467355f8c15a9e3212b9d7118e
pmock2 <payton.mock@gmail.com>
2019-01-10 18:59:59 -0500

    Added sign up and log in pages

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <navigation-header></navigation-header>
-    <v-content transition="slide-x-transition">
+    <v-content transition="slide-x-transition" style="background: radial-gradient(circle, rgb(46, 69, 90), rgb(17, 30, 42)) no-repeat;">
       <router-view></router-view>
     </v-content>
   </v-app>
--- src/views/Signin.vue
+++ src/views/Signin.vue
@@ -1,6 +1,26 @@
 <template>
-  <div>
-    <h1>This is sign in page</h1>
+  <div class="login-container">
+    <div class="tab-content">
+      <div class="login-child-container">
+        <h1 class="login-title">Welcome Back!</h1>
+        <div class="login-input-field">
+          <label class="login-label">
+            Email<span class="required-star">*</span>
+          </label>
+          <input type="text" id="email" maxlength="88" autocomplete="off" class="login-input">
+        </div>
+        <div class="login-input-field">
+          <label class="login-label">
+            Password<span class="required-star">*</span>
+          </label>
+          <input type="password" id="password" maxlength="100000" autocomplete="off" class="login-input">
+        </div>
+        <p class="forgot"><a class="login-link" href="forgot_pass.php">Forgot Password?</a></p>
+        <p><a class="login-link" href="/signup.html"><b>Not a member?</b></a></p>
+        <button id="loginbtn" class="login-button">Log In</button>
+        <span id="status"></span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,3 +30,113 @@ export default {
   components: {}
 };
 </script>
+
+<style>
+  .login-container {
+    background: rgba(0,0,0,0.8);
+    border-radius: 2px;
+    box-shadow: 0 0 0 5px rgba(0,0,0,0.8);
+    padding: 40px;
+    max-width: 600px;
+    display: flex;
+    height: 100%;
+    position: fixed;
+    justify-content: center;
+    width: 100%;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  .tab-content {
+    min-height: 420px;
+    padding: 50px 0;
+  }
+
+  .login-child-container {
+    position: absolute;
+    top: 40%;
+    left: 50%;
+    transform: translateX(-50%) translateY(-50%);
+    width: 75%;
+  }
+
+  .login-title {
+    text-align: center;
+    color: #ffffff;
+    font-weight: 300;
+    margin: 0 0 40px;
+    font-size: 32px;
+    box-sizing: border-box;
+  }
+
+  .login-input-field {
+    position: relative;
+    margin-bottom: 40px;
+  }
+
+  .login-label {
+    position: absolute;
+    transform: translateY(6px);
+    left: 3px;
+    color: rgba(255, 255, 255, 0.5);
+    transition: all 0.25s ease;
+    pointer-events: none;
+    font-size: 22px;
+    top: 6px;
+  }
+
+  .required-star {
+    margin: 2px;
+    color: #0566ad;
+  }
+
+  .login-input {
+    font-size: 22px;
+    display: block;
+    width: 100%;
+    height: 100%;
+    padding: 10px;
+    background: none;
+    border: 1px solid #a0b3b0;
+    color: #ffffff;
+    border-radius: 0;
+    transition: border-color .25s ease, box-shadow .25s ease;
+    margin-left: -10px;
+  }
+
+  .forgot {
+    margin-top: -30px;
+    text-align: right;
+  }
+
+  .login-button {
+    width: 100%;
+    border: 0;
+    outline: auto;
+    outline-color: #0566ad;
+    border-radius: 0;
+    padding: 15px 0;
+    font-size: 2rem;
+    font-weight: 600;
+    text-transform: uppercase;
+    letter-spacing: .1em;
+    background: black;
+    color: #ffffff;
+    -webkit-transition: all 0.5s ease;
+    transition: all 0.5s ease;
+    -webkit-appearance: none;
+    cursor: pointer;
+  }
+
+  .login-button:hover {
+    background: #005BB3;
+  }
+
+  .login-link {
+    color: #179b77;
+    border: 0;
+    outline: 0;
+    text-decoration: none;
+    font-weight: bold;
+  }
+</style>
