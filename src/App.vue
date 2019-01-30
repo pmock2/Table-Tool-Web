5a5cf375ca7030adcd0a89adc58966cee33ca289
spetsing <spetsing@mit.edu>
2019-01-30 09:12:35 -0500

    Removing vue/prettier form eslint config

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <v-app>
     <navigation-header></navigation-header>
-    <v-content transition="slide-x-transition" style="background: radial-gradient(circle, rgb(46, 69, 90), rgb(17, 30, 42)) no-repeat;">
+    <v-content
+      transition="slide-x-transition"
+      style="background: radial-gradient(circle, rgb(46, 69, 90), rgb(17, 30, 42)) no-repeat;"
+    >
       <router-view></router-view>
     </v-content>
   </v-app>
