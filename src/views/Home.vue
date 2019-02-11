56275c5e55f1e6d3deafca19e6d87347d742f2a2
cleikness <clayton.leikness@gmail.com>
2019-02-10 20:03:04 -0500

    Add files via upload

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,24 +7,75 @@
         <v-btn class="get-started-button" to="/join">Get Started</v-btn>
       </div>
     </div>
-    <div class="page-section">
-      <div class="page-section-title">My Title</div>
+    <div class="page-section, text-xs-center">
+        <div class="button_links" style="background: linear-gradient( #476892, #4c14d0) no-repeat;"> 
+            <div class="page-section-title">Check these out!</div> 
+        <br> 
+        <br>
+        <br>
+        <v-btn color ="error" large round="button-A" >Button A</v-btn>
+        <v-btn color ="error" large round="button-B" >Button B</v-btn>
+        <v-btn color ="error" large round="button-C" >Button C</v-btn>
+        <v-btn color ="error" large round="button-D" >Button D</v-btn>
+        </div>
       <div class="page-section-content">
       </div>
     </div>
-    <div class="page-section">
-      <div class="page-section-title">My Title</div>
-      <div class="page-section-content">
-      </div>
+    <div class="page-info">
+      <v-footer
+    height="auto"
+    color="primary lighten-1"
+  >
+    <v-layout
+      justify-center
+      row
+      wrap
+    >
+      <v-btn
+        v-for="link in links"
+        :key="link"
+        color="white"
+        flat
+        round
+      >
+        {{ link }}
+      </v-btn>
+      <v-flex
+        primary
+        lighten-2
+        py-3
+        text-xs-center
+        white--text
+        xs12
+      >
+        &copy;2019 — <strong>Vuetify</strong>
+      </v-flex>
+    </v-layout>
+  </v-footer>
     </div>
-  </div>
-</template>
+</div>
+  </template>
 
 <script>
 export default {
   name: "home",
   components: {}
 };
+</script>
+
+<script>
+ export default {
+    data: () => ({
+      links: [
+        'Home',
+        'About Us',
+        'Team',
+        'Services',
+        'Blog',
+        'Contact Us'
+      ]
+    })
+  }
 </script>
 
 <style>
@@ -88,15 +139,14 @@ export default {
 }
 
 .page-section {
-    min-height: 400px;
-    background: white;
+    min-height: 250px;
     position: relative;
     border-top: 1px solid #aaa;
     border-bottom: 1px solid #aaa;
 }
 
 .page-section-title {
-    font-size: 2.8vw;
+    font-size: 2.0vw;
     color: #444; 
     position: relative;
     text-align: center;
@@ -112,19 +162,120 @@ export default {
     top: 10vh;
 }
 
+/*.button-A {
+    margin-top: 8%;
+    text-align: center;
+    font-size: 2.5vh !important;
+    height: auto;
+    width: fit-content;
+    background: #194d33 !important;
+    cursor: pointer;
+    padding: 2vh 3vh;
+    border-radius: 4px;
+    color: rgba(255,255,255,0.9) !important;
+    transition: opacity 1.5s, color 0.2s, box-shadow 0.2s;
+    box-shadow: 0 1px 8px 1px rgba(0,0,0,0.7) !important;
+}
+
+.button-A:hover {
+    color: #194d3e !important;
+    box-shadow: 0 3px 10px 2px rgba(0,0,0,0.6) !important;
+}
+
+.button-B {
+    margin-top: 8%;
+    text-align: center;
+    font-size: 2.5vh !important;
+    height: auto;
+    width: fit-content;
+    background: #194d33 !important;
+    cursor: pointer;
+    padding: 2vh 3vh;
+    border-radius: 4px;
+    color: rgba(255,255,255,0.9) !important;
+    transition: opacity 1.5s, color 0.2s, box-shadow 0.2s;
+    box-shadow: 0 1px 8px 1px rgba(0,0,0,0.7) !important;
+}
+
+.button-B:hover {
+    color: white !important;
+    box-shadow: 0 3px 10px 2px rgba(0,0,0,0.6) !important;
+}
+
+.button-C {
+    margin-top: 8%;
+    text-align: center;
+    font-size: 2.5vh !important;
+    height: auto;
+    width: fit-content;
+    background: #194d33 !important;
+    cursor: pointer;
+    padding: 2vh 3vh;
+    border-radius: 4px;
+    color: rgba(255,255,255,0.9) !important;
+    transition: opacity 1.5s, color 0.2s, box-shadow 0.2s;
+    box-shadow: 0 1px 8px 1px rgba(0,0,0,0.7) !important;
+}
+
+.button-C:hover {
+    color: white !important;
+    box-shadow: 0 3px 10px 2px rgba(0,0,0,0.6) !important;
+}
+
+.button-D {
+    margin-top: 8%;
+    text-align: center;
+    font-size: 2.5vh !important;
+    height: auto;
+    width: fit-content;
+    background: #194d33 !important;
+    cursor: pointer;
+    padding: 2vh 3vh;
+    border-radius: 4px;
+    color: rgba(255,255,255,0.9) !important;
+    transition: opacity 1.5s, color 0.2s, box-shadow 0.2s;
+    box-shadow: 0 1px 8px 1px rgba(0,0,0,0.7) !important;
+}
+
+.button-D:hover {
+    color: white !important;
+    box-shadow: 0 3px 10px 2px rgba(0,0,0,0.6) !important;
+}
+
+.page-info {
+    min-height: 400px;
+    position: relative;
+    border-top: 1px solid #aaa;
+    border-bottom: 1px solid #aaa;
+}
+
+.page-info-title {
+    font-size: 1vw;
+    color: #444; 
+    position: relative;
+    text-align: center;
+    top: 5vh;
+}
+
+.page-info-content {
+  font-size: 1vw;
+  color: #444;  
+  position: relative;
+  text-align: center;
+  top: 5vh;
+}
+*/
 *::-webkit-scrollbar {
-    width: 6px !important;
-    height: 6px !important;
-    border-radius: 5px !important;
-  }
-  
-  *::-webkit-scrollbar-thumb {
-    background-color: #4c535d !important;
-    border-radius: 5px !important;
-  }
-  
-  *::-webkit-scrollbar-track {
-    background-color: #1b232d !important;
-    border-radius: 5px !important;
-  }
+  width: 6px !important;
+  height: 6px !important;
+  border-radius: 5px !important;
+}
+*::-webkit-scrollbar-thumb {
+  background-color: #4c535d !important;
+  border-radius: 5px !important;
+}
+*::-webkit-scrollbar-track {
+  background-color: #1b232d !important;
+  border-radius: 5px !important;
+}
 </style>
